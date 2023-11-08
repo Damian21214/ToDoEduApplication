141b4267377c1921822d19f26f47d3bb3b7c8bba
Damian Langer <damian.langer@gmail.com>
2023-11-08 22:26:58 +0100

    Initial commit

---
+++ src/main/java/com/sap/langer/edu/todoeduapplication/ToDoEduApplication.java
@@ -0,0 +1,13 @@
+package com.sap.langer.edu.todoeduapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sap/langer/edu/todoeduapplication/ToDoEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.langer.edu.todoeduapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
