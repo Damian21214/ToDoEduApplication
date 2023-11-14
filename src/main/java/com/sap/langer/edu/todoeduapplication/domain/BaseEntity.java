580fe58a55dce6378552bd3ffb986b626acbbb54
Damian Langer <damian.langer@gmail.com>
2023-11-14 22:56:57 +0100

    BaseEntity as MappedSuperclass

--- src/main/java/com/sap/langer/edu/todoeduapplication/domain/BaseEntity.java
+++ src/main/java/com/sap/langer/edu/todoeduapplication/domain/BaseEntity.java
@@ -1,13 +1,45 @@
 package com.sap.langer.edu.todoeduapplication.domain;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime created;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime modified;
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @PostPersist
+    private void created() {
+        this.created = LocalDateTime.now();
+        this.modified = this.created;
+    }
+
+    @PostUpdate
+    private void updated() {
+        this.modified = LocalDateTime.now();
+    }
 }
