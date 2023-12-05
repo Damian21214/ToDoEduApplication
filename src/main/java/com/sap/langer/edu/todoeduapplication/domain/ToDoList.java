0cb4ba6364451a6ec19b279e9a16388411b81628
Damian Langer <damian.langer@gmail.com>
2023-12-05 22:33:40 +0100

    JPA mapping of domain entities

--- src/main/java/com/sap/langer/edu/todoeduapplication/domain/ToDoList.java
+++ src/main/java/com/sap/langer/edu/todoeduapplication/domain/ToDoList.java
@@ -1,16 +1,49 @@
 package com.sap.langer.edu.todoeduapplication.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class ToDoList extends BaseEntity {
+    @Basic
+    @Column(length = 255, nullable = false)
     private String title;
+
+    @Enumerated(EnumType.STRING)
     private ToDoListStatus status;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime statusChanged;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "toDoList")
+    @ToString.Exclude
     private List<ToDoTask> tasks;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ToDoList toDoList = (ToDoList) o;
+        return getId() != null && Objects.equals(getId(), toDoList.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/sap/langer/edu/todoeduapplication/domain/ToDoTask.java
+++ src/main/java/com/sap/langer/edu/todoeduapplication/domain/ToDoTask.java
@@ -1,18 +1,67 @@
 package com.sap.langer.edu.todoeduapplication.domain;
 
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class ToDoTask extends BaseEntity {
+    @Basic
+    @Column(length = 64, nullable = false)
     private String title;
+
+    @Basic
+    @Column(length = 1024, nullable = true)
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private ToDoStatus status;
+
+    @Basic
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime statusChanged;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime scheduled;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private ToDoList toDoList;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ToDoTask toDoTask)) return false;
+
+        if (!Objects.equals(title, toDoTask.title)) return false;
+        if (!Objects.equals(description, toDoTask.description))
+            return false;
+        if (status != toDoTask.status) return false;
+        if (!Objects.equals(statusChanged, toDoTask.statusChanged))
+            return false;
+        if (!Objects.equals(scheduled, toDoTask.scheduled)) return false;
+        return Objects.equals(toDoList, toDoTask.toDoList);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (statusChanged != null ? statusChanged.hashCode() : 0);
+        result = 31 * result + (scheduled != null ? scheduled.hashCode() : 0);
+        result = 31 * result + (toDoList != null ? toDoList.hashCode() : 0);
+        return result;
+    }
 }
